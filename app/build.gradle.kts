1f04fd2ff2243c1393f342801a18ebe601055771
galhrrera <juan.galeanoherrera@gmail.com>
2023-10-10 22:26:23 -0500

    Created new abb with new version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "co.upb.taskyo"
         minSdk = 34
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.1"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
